cdc43bdff4403bb9bae630bb76337c0445cc002b
EfimiaPanagiotaki-StreetDrone <40384066+EfimiaPanagiotaki-StreetDrone@users.noreply.github.com>
2020-10-27 19:29:55 +0000

    Kinetic Release 1.2.0 (#26)
    * set ROS hostname to localhost (#15)
    
    Thanks @P4nos 
    This makes sense for single machine configuration for local network setup. The roscore should initialise correctly with this fix.
    
    * Added offset in VI Control Loop
    
    Changes made
    Added offset in VI control loop to fix bug18
    Updated some comments in VI
    Fixed gps_imu param name
    
    * Feature/17 redesign full stack with best practices guidelines (#21)
    
    * Code Redesigned
    
    * Bring branch up-to-date
    
    * Fix VI conflict
    
    * Update submodule to latest commit
    
    Co-authored-by: EfimiaPanagiotaki-StreetDrone <efimia@streetdrone.com>
    Co-authored-by: P4nos <P4nos@users.noreply.github.com>
    Co-authored-by: Fionan-StreetDrone <fionan@streetdrone.com>
    Co-authored-by: AbdelrahmanBarghouth-StreetDrone <58561901+AbdelrahmanBarghouth-StreetDrone@users.noreply.github.com>
    Co-authored-by: EfimiaPanagiotaki-StreetDrone <efimia@streetdrone.com>

--- src/supervisor/check_message_alive/src/check_message_alive_main.cpp
+++ src/supervisor/check_message_alive/src/check_message_alive_main.cpp
@@ -29,29 +29,11 @@
  */
 
 
-#include <ros/ros.h>
-#include <diagnostic_msgs/DiagnosticArray.h>
-#include <diagnostic_msgs/DiagnosticStatus.h>
-#include "error_listener.h"
-
-void error_listener::diagnostics_callback(const diagnostic_msgs::DiagnosticArray::ConstPtr& msg)
+#include "ros/ros.h"
+#include "check_message_alive.h"
+int main(int argc, char **argv)
 {
-    for(int topic = 0; topic < (msg->status).size(); topic++)
-    {
-        if(msg->status[topic].level == 2)
-        {
-            ROS_ERROR("Error in %s", msg->status[topic].name.c_str());
-        }
-    }
-}
-
-int error_listener::RUN(int argc, char **argv)
-{
-    ros::init(argc, argv, "read_diagnostics");
-
-    ros::NodeHandle nh("~");
-    ros::NodeHandle private_nh("~");
-    
-    ros::Subscriber aslan_diagnostics_sub = nh.subscribe<diagnostic_msgs::DiagnosticArray>("/aslan_diagnostics", 1, &error_listener::diagnostics_callback, this);
-    while(ros::ok()) ros::spinOnce();
+	check_message_alive diag_msg;
+	diag_msg.RUN(argc, argv);
+	return 0;
 }
